71d1a74cc0ef7f18c24184fd26da60d56f64652c
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-12-30 17:32:26 +0700

    add method while in public void search

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -62,7 +62,18 @@ namespace BinarySearchTree
         }
         public void search(string element, ref Node parent, ref Node currentNode)
         {
-
+            /*This function searches the currentNode of the specified Node as
+             well as the current Node of its parent*/
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else 
+                    currentNode = currentNode.rightchild;
+            }
         }
     }
     internal class Program
