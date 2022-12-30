aa4292f0e78dc11cebab502c405aa2f4aa68690f
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-12-30 17:13:49 +0700

    Add public Node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,6 +9,15 @@ namespace BinarySearchTree
 
    class Node
     {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+
+        }
 
     }
     internal class Program
