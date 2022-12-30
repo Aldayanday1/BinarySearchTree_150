86c20f704845612f881ba9a81da55f3967e27cd9
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-12-30 17:50:10 +0700

    Add switch case 2

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -143,6 +143,11 @@ namespace BinarySearchTree
                             x.insert(word);
                         }
                         break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
                 }
 
             }
